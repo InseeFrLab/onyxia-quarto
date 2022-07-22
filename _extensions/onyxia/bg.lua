930431822a2fe3678734fc8ae56985163653f61d
Romain Lesur <romain.lesur@gmail.com>
2022-07-22 22:47:40 +0200

    fix path

--- _extensions/onyxia/bg.lua
+++ _extensions/onyxia/bg.lua
@@ -1,6 +1,9 @@
 function Header(el)
+    backgroundImagePath = quarto.utils.resolvePath('background.svg')
+    quarto.doc.addFormatResource(backgroundImagePath)
     if not el.attributes['background-image'] then
-        el.attributes['background-image'] = '_extensions/RLesur/onyxia/background.svg'
+        el.attributes['background-image'] = 'background.svg'
+        el.attributes['background-size'] = 'contain'
     end
 
     return el
